910f8d8d76f6aaa6600e3a1230c6b05320c358ab
Alex Emelyanov <alex84_84_84@mail.ru>
2021-05-07 15:17:35 +0300

    fix proj ID

--- src/main.py
+++ src/main.py
@@ -44,7 +44,7 @@ def export_only_labeled_items(api: sly.Api, task_id, context, state, app_logger)
     RESULT_ARCHIVE_PATH = os.path.join(my_app.data_dir, RESULT_DIR_NAME)
     ARCHIVE_NAME = '{}_{}.tar.gz'.format(PROJECT_ID, project_name)
     RESULT_ARCHIVE = os.path.join(my_app.data_dir, ARCHIVE_NAME)
-    remote_archive_path = "/{}/{}_{}_{}".format(RESULT_DIR_NAME, TASK_ID, ARCHIVE_NAME)
+    remote_archive_path = "/{}/{}_{}".format(RESULT_DIR_NAME, TASK_ID, ARCHIVE_NAME)
     if api.file.exists(TEAM_ID, remote_archive_path):
         logger.warn('Archive with name {} already exist in {} folder'.format(ARCHIVE_NAME, RESULT_DIR_NAME))
         my_app.stop()
